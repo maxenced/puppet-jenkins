ea32f3314bef9a600e11635615080bb19349500c
Maxence Dunnewind <maxence@dunnewind.net>
2013-02-15 16:49:17 +0100

    add git and subversion, add jenkins ssh key

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,6 +3,33 @@ class jenkins::package($version = 'installed') {
     'jenkins' :
       ensure => $version;
   }
+
+  package { 'git':
+    ensure => present
+  }
+
+  package { 'subversion':
+    ensure => present
+  }
+
+  file { '/home/jenkins':
+    ensure  => directory,
+    owner   => 'jenkins',
+    mode    => '0750',
+    require => Package['jenkins']
+  }
+
+  file { '/home/jenkins/.ssh':
+    ensure  => directory,
+    owner   => 'jenkins',
+    mode    => '0700'
+  }
+
+  file { '/home/jenkins/.ssh/id_rsa':
+    ensure  => file,
+    content => file('/etc/puppet/secrets/e2cbot.priv'),
+    mode    => '0600'
+  }
 }
 
 # Note:  Jenkins should install java, but it doesn't.  You may have to do it on your own.
